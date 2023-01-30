b3836bb481f3bfbd9599e0ddf0e5186306c6cd1e
Pawel Urasinski <uraspawel@gmail.com>
2023-01-30 21:39:45 +0100

    Made admin panel page
    Added admin panel view layout. Updated adminRouter, made admin.css file

--- routers/admin.ts
+++ routers/admin.ts
@@ -5,6 +5,10 @@ export const adminRouter = Router();
 const login = 'admin';
 const password = '123';
 
+let counter = 1;
+
+const workers: [{}] = [{}];
+
 adminRouter
 	.get('/', async (req: Request, res: Response): Promise<void> => {
 		res.render('admin/admin-login', {
@@ -19,4 +23,21 @@ adminRouter
 		} else {
 			res.redirect('/admin');
 		}
+	})
+	.get('/panel', async (req: Request, res: Response): Promise<void> => {
+		res.render('admin/admin-panel', {
+			style: 'admin.css',
+			workers,
+		});
+	})
+
+	.post('/panel', async (req: Request, res: Response): Promise<void> => {
+		workers.push({
+			id: counter++,
+			name: req.body.workerFirstname,
+			lastname: req.body.workerLastname,
+			machine: req.body.machine,
+		});
+
+		res.redirect('/admin/panel');
 	});
