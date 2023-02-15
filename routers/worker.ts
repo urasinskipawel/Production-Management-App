c7c14899668acb647bbe1093b234e49ca3fd2dd4
Pawel Urasinski <uraspawel@gmail.com>
2023-02-15 21:07:01 +0100

    Made worker panel page
    Added worker panel view layout. Updated workerRouter. Used admin.css file

--- routers/worker.ts
+++ routers/worker.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { progressStatuses } from '../utils/constants';
 
 export const workerRouter = Router();
 
@@ -10,5 +11,18 @@ workerRouter
 	})
 
 	.post('/', async (req: Request, res: Response): Promise<void> => {
-		res.redirect('/worker/panel/');
+		const workerName: string = encodeURIComponent(req.body.name);
+		const workerLastname: string = encodeURIComponent(req.body.lastname);
+		res.redirect(`/worker/panel/${workerName}/${workerLastname}`);
+	})
+
+	.get('/panel/:workerName/:workerLastname', async (req: Request, res: Response): Promise<void> => {
+		const workerName: string = req.params.workerName;
+		const workerLastname: string = req.params.workerLastname;
+		res.render('worker/worker-panel', {
+			style: 'admin.css',
+			progressStatuses,
+			workerName,
+			workerLastname,
+		});
 	});
