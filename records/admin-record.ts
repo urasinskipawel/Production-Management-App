2ab743e9f1e401e4fad7d12c0bf250324a51b3ea
Pawel Urasinski <uraspawel@gmail.com>
2023-02-17 21:10:18 +0100

    Made admin-record file
    Created class AdminRecord contains methods to get data from database

---
+++ records/admin-record.ts
@@ -0,0 +1,36 @@
+import { pool } from '../config/db';
+// import { ValidationError } from '../utils/errors';
+import { v4 as uuid } from 'uuid';
+import { FieldPacket } from 'mysql2';
+
+type AdminRecordResult = [AdminRecord[], FieldPacket[]];
+
+export class AdminRecord {
+	id?: string;
+	firstname: string;
+	lastname: string;
+
+	constructor(obj: AdminRecord) {
+		this.id = obj.id;
+		this.firstname = obj.firstname;
+		this.lastname = obj.lastname;
+	}
+
+	async insert(): Promise<string> {
+		if (!this.id) {
+			this.id = uuid();
+		}
+
+		await pool.execute('INSERT INTO `workers`(`id`, `firstname`, `lastname`) VALUES(:id, :firstname,:lastname)', {
+			id: this.id,
+			firstname: this.firstname,
+			lastname: this.lastname,
+		});
+
+		return this.id;
+	}
+	static async listAll(): Promise<AdminRecord[]> {
+		const [results] = (await pool.execute('SELECT * FROM `workers`')) as AdminRecordResult;
+		return results.map(obj => new AdminRecord(obj));
+	}
+}
