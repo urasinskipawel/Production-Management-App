61cf5d62de60d0f4e7eeb55fa0a49422e6512e3e
Pawel Urasinski <uraspawel@gmail.com>
2023-02-21 17:44:43 +0100

    Updated AdminRecord
    Added properties to class constructor. Improved database query methods

--- records/admin-record.ts
+++ records/admin-record.ts
@@ -9,11 +9,21 @@ export class AdminRecord {
 	id?: string;
 	firstname: string;
 	lastname: string;
+	machinesId: string;
+	tasksId: string;
+	machine: string;
+	drawing: string;
+	project: string;
 
 	constructor(obj: AdminRecord) {
 		this.id = obj.id;
 		this.firstname = obj.firstname;
 		this.lastname = obj.lastname;
+		this.machinesId = obj.machinesId;
+		this.tasksId = obj.tasksId;
+		this.machine = obj.machine;
+		this.drawing = obj.drawing;
+		this.project = obj.project;
 	}
 
 	async insertWorker(): Promise<string> {
@@ -21,16 +31,22 @@ export class AdminRecord {
 			this.id = uuid();
 		}
 
-		await pool.execute('INSERT INTO `workers`(`id`, `firstname`, `lastname`) VALUES(:id, :firstname,:lastname)', {
-			id: this.id,
-			firstname: this.firstname,
-			lastname: this.lastname,
-		});
+		await pool.execute(
+			'INSERT INTO `workers`(`id`, `firstname`, `lastname`, `machinesId`,  ) VALUES (:id, :firstname, :lastname, (SELECT `id` FROM `machines` WHERE `machine` = :machine) )',
+			{
+				id: this.id,
+				firstname: this.firstname,
+				lastname: this.lastname,
+				machine: this.machine,
+			}
+		);
 
 		return this.id;
 	}
 	static async listAllWorkers(): Promise<AdminRecord[]> {
-		const [results] = (await pool.execute('SELECT * FROM `workers`')) as AdminRecordResult;
+		const [results] = (await pool.execute(
+			'SELECT `workers`.`id`,`workers`.`firstname`, `workers`.`lastname`, `machines`.`machine`FROM `workers` JOIN `machines` ON `workers`.`machinesId` = `machines`.`id`'
+		)) as AdminRecordResult;
 		return results.map(obj => new AdminRecord(obj));
 	}
 }
