560143f18b624c156b4083a13d5bcf4dce8cb7df
Pawel Urasinski <uraspawel@gmail.com>
2023-02-21 18:05:39 +0100

    Added login validation to access worker panel
    Created validation to check if user exists.
    The router leads to the individual panel of the logged-in employee

--- routers/worker.ts
+++ routers/worker.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { AdminRecord } from '../records/admin-record';
 import { progressStatuses } from '../utils/constants';
 
 export const workerRouter = Router();
@@ -11,18 +12,30 @@ workerRouter
 	})
 
 	.post('/', async (req: Request, res: Response): Promise<void> => {
-		const workerName: string = encodeURIComponent(req.body.name);
-		const workerLastname: string = encodeURIComponent(req.body.lastname);
-		res.redirect(`/worker/panel/${workerName}/${workerLastname}`);
+		const workersList = await AdminRecord.listAllWorkers();
+
+		const foundLoginData = workersList.find(a => a.firstname === req.body.firstname && a.lastname === req.body.lastname);
+
+		if (foundLoginData) {
+			if (foundLoginData.firstname && foundLoginData.lastname) {
+				const workerFirstName: string = encodeURIComponent(req.body.firstname);
+				const workerLastname: string = encodeURIComponent(req.body.lastname);
+				res.redirect(`/worker/panel/${workerFirstName}/${workerLastname}`);
+			}
+		} else {
+			res.redirect('/worker');
+		}
 	})
 
-	.get('/panel/:workerName/:workerLastname', async (req: Request, res: Response): Promise<void> => {
-		const workerName: string = req.params.workerName;
+	.get('/panel/:workerFirstName/:workerLastname', async (req: Request, res: Response): Promise<void> => {
+		const workerFirstName: string = req.params.workerFirstName;
 		const workerLastname: string = req.params.workerLastname;
 		res.render('worker/worker-panel', {
 			style: 'admin.css',
 			progressStatuses,
-			workerName,
+			workerFirstName,
 			workerLastname,
 		});
 	});
+
+// TODO4 : Zrobic listowanie zadan workersa w panelu workers
