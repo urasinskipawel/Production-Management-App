b21e57c34a93ad62956706ed5cf536a98c961424
Pawel Urasinski <uraspawel@gmail.com>
2023-03-05 23:39:29 +0100

    Joined new features to hbs-helpers
    Created new functions needed to handle views

--- utils/handlebars-helpers.ts
+++ utils/handlebars-helpers.ts
@@ -1,5 +1,13 @@
 export const handlebarsHelpers = {
-	showShortId: (a: string): any => {
-		return `${a.substring(0, 8)}...`;
+	showShortId: (a: string): string => `${a.substring(0, 8)}...`,
+	getCurrentTime: (): string => {
+		let date_ob = new Date();
+		let hours: number = date_ob.getHours();
+		let minutes: number = date_ob.getMinutes();
+		let seconds: number = date_ob.getSeconds();
+		return `${hours}:${minutes}:${seconds}`;
 	},
+	changeToUppercase: (a: string): string => a.toUpperCase(),
+	firstCharToUppercase: (a: string): string => `${a.charAt(0).toUpperCase()}${a.slice(1).toLowerCase()}`,
+	equals: (a: string, b: string): boolean => a === b,
 };
